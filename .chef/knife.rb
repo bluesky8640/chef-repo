18551a10f6e53bc98cb67279c53c67bfd6f8462f
bluesky8640 <bluesky8640@126.com>
2014-01-06 14:02:02 +0000

    Initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'root'
+client_key               '/root/chef-repo/.chef/root.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://saasmaster.saasservicer.h2.internal.cloudapp.net:443'
+syntax_check_cache_path  '/root/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/root/chef-repo/cookbooks' ]
