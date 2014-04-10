f1e1e76240c1bbed6b84d646aba3d6183434edce
bluesky8640 <bluesky8640@126.com>
2014-04-10 19:49:22 +0800

    update bowtie

--- cookbooks/bowtie/attributes/default.rb
+++ cookbooks/bowtie/attributes/default.rb
@@ -1,6 +1,10 @@
 # Current user
 default['current_user'] = 'hadoop'
 
+# Bio-soft home
+default['bio_soft_home'] = '/bio-soft'
+
+
 # Bowtie package URI
 default['bowtie_package_uri'] = 'http://jaist.dl.sourceforge.net/project/bowtie-bio/bowtie/1.0.1/bowtie-1.0.1-linux-x86_64.zip'
 
@@ -9,6 +13,3 @@ default['bowtie_package'] = 'bowtie-1.0.1-linux-x86_64.zip'
 
 # Bowtie dir
 default['bowtie_dir'] = 'bowtie-1.0.1'
-
-# Bio-soft home
-default['bio_soft_home'] = '/bio-soft'
